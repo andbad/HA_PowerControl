99d8f3426d073741472500df3f2f22de82a59021
andbad <andbad@iol.it>
2021-01-25 12:23:02 +0100

    Add files via upload

---
+++ update_entities.py
@@ -0,0 +1,78 @@
+#group_entities = hass.states.get('group.all_lights').attributes['entity_id']
+#all_lights = []
+#for e in group_entities:
+#    all_lights.append(e)
+#service_data = {'entity_id': 'input_select.timer_generico7',
+#                'options': all_lights}
+#hass.services.call('input_select', 'set_options', service_data)
+
+
+#group_entities = hass.states.get('group.all_lights').attributes['entity_id']
+#all_lights = []
+#for e in group_entities:
+#    all_lights.append(hass.states.get(e).attributes['friendly_name'])
+#service_data = {'entity_id': 'input_select.timer_generico7',
+#                'options': all_lights}
+#hass.services.call('input_select', 'set_options', service_data)
+
+
+#entities = hass.states.entity_ids()
+#service_data = {'entity_id': 'input_select.entities', 'options': sorted(entities)}
+#hass.services.call('input_select', 'set_options', service_data)
+
+
+
+entities = hass.states.entity_ids('switch')
+all_switches = ["Seleziona"]
+for e in entities:
+    all_switches.append(e)
+
+service_data = {'entity_id': 'input_select.carico_1_switch', 'options': sorted(all_switches)}
+hass.services.call('input_select', 'set_options', service_data)
+service_data = {'entity_id': 'input_select.carico_2_switch', 'options': sorted(all_switches)}
+hass.services.call('input_select', 'set_options', service_data)
+service_data = {'entity_id': 'input_select.carico_3_switch', 'options': sorted(all_switches)}
+hass.services.call('input_select', 'set_options', service_data)
+service_data = {'entity_id': 'input_select.carico_4_switch', 'options': sorted(all_switches)}
+hass.services.call('input_select', 'set_options', service_data)
+service_data = {'entity_id': 'input_select.carico_5_switch', 'options': sorted(all_switches)}
+hass.services.call('input_select', 'set_options', service_data)
+service_data = {'entity_id': 'input_select.carico_6_switch', 'options': sorted(all_switches)}
+hass.services.call('input_select', 'set_options', service_data)
+service_data = {'entity_id': 'input_select.carico_7_switch', 'options': sorted(all_switches)}
+hass.services.call('input_select', 'set_options', service_data)
+service_data = {'entity_id': 'input_select.carico_8_switch', 'options': sorted(all_switches)}
+hass.services.call('input_select', 'set_options', service_data)
+service_data = {'entity_id': 'input_select.carico_9_switch', 'options': sorted(all_switches)}
+hass.services.call('input_select', 'set_options', service_data)
+service_data = {'entity_id': 'input_select.carico_10_switch', 'options': sorted(all_switches)}
+hass.services.call('input_select', 'set_options', service_data)
+
+entities = hass.states.entity_ids('sensor')
+all_sensor = ["Seleziona"]
+for e in entities:
+    all_sensor.append(e)
+
+service_data = {'entity_id': 'input_select.carico_1_potenza', 'options': sorted(all_sensor)}
+hass.services.call('input_select', 'set_options', service_data)
+service_data = {'entity_id': 'input_select.carico_2_potenza', 'options': sorted(all_sensor)}
+hass.services.call('input_select', 'set_options', service_data)
+service_data = {'entity_id': 'input_select.carico_3_potenza', 'options': sorted(all_sensor)}
+hass.services.call('input_select', 'set_options', service_data)
+service_data = {'entity_id': 'input_select.carico_4_potenza', 'options': sorted(all_sensor)}
+hass.services.call('input_select', 'set_options', service_data)
+service_data = {'entity_id': 'input_select.carico_5_potenza', 'options': sorted(all_sensor)}
+hass.services.call('input_select', 'set_options', service_data)
+service_data = {'entity_id': 'input_select.carico_6_potenza', 'options': sorted(all_sensor)}
+hass.services.call('input_select', 'set_options', service_data)
+service_data = {'entity_id': 'input_select.carico_7_potenza', 'options': sorted(all_sensor)}
+hass.services.call('input_select', 'set_options', service_data)
+service_data = {'entity_id': 'input_select.carico_8_potenza', 'options': sorted(all_sensor)}
+hass.services.call('input_select', 'set_options', service_data)
+service_data = {'entity_id': 'input_select.carico_9_potenza', 'options': sorted(all_sensor)}
+hass.services.call('input_select', 'set_options', service_data)
+service_data = {'entity_id': 'input_select.carico_10_potenza', 'options': sorted(all_sensor)}
+hass.services.call('input_select', 'set_options', service_data)
+
+service_data = {'entity_id': 'input_select.potenza_carichi', 'options': sorted(all_sensor)}
+hass.services.call('input_select', 'set_options', service_data)
